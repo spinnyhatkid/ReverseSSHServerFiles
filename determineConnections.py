9b4f909506c46a26e50b86149390f0e6371e0e75
Dustin Sulek <handicapparkingg@gmail.com>
2016-06-21 23:49:13 -0400

    update ssshp file within range

--- determineConnections.py
+++ determineConnections.py
@@ -2,6 +2,9 @@
 
 import subprocess
 
+min_port = 29000
+max_port = 29999
+
 def bash_call(cmd):
     return subprocess.call(['/bin/bash', '-c', cmd])
 
@@ -15,6 +18,7 @@ ports = []
 try:
     b = bash_command("netstat -l | grep tcp | grep LISTEN | grep '*:[[:digit:]]'").splitlines()
     ports = map(lambda line: line.split()[3][2:], b)
+    ports = filter(lambda port: int(port) >= min_port and int(port) <= max_port, ports)
 except:
     ports = existing
 
